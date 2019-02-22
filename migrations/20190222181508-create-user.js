80ec449f02a2ca8274b93c532894030b0ebfec54
ariebrainware <publicu001@gmail.com>
2019-02-23 02:21:05 +0700

    Add user models migration

---
+++ migrations/20190222181508-create-user.js
@@ -0,0 +1,38 @@
+'use strict'
+// For database model, we dont really to implement to many details.
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        isEmail: true,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('users')
+  },
+}
